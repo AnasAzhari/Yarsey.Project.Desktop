562473ef3cf9cf4cd54f0c8648c02ee942949d4f
Anasza85 <4k.anas85@gmail.com>
2021-03-14 08:03:02 +0800

    migration and appsettings.json

--- Yarsey.WPF/App.xaml.cs
+++ Yarsey.WPF/App.xaml.cs
@@ -5,6 +5,13 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Yarsey.WPF.HostBuilder;
+using Yarsey.EntityFramework;
+
 
 namespace Yarsey.WPF
 {
@@ -13,5 +20,34 @@ namespace Yarsey.WPF
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = CreateHostBuilder().Build();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args = null)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .AddConfiguration()
+                .AddDbContext(); 
+                
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+            YarseyDbContextFactory contextFactory = _host.Services.GetRequiredService<YarseyDbContextFactory>();
+            using (YarseyDbContext context=contextFactory.CreateDbContext())
+            {
+                context.Database.Migrate();
+            }
+
+
+            base.OnStartup(e);
+        }
+
+
     }
 }
---
+++ Yarsey.WPF/HostBuilder/AddDbContextHostBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Yarsey.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yarsey.WPF.HostBuilder
+{
+    public static class AddDbContextHostBuilderExtensions
+    {
+        public static IHostBuilder AddDbContext(this IHostBuilder host)
+        {
+            host.ConfigureServices((context, services) =>
+            {
+                string connectionString = context.Configuration.GetConnectionString("sqlite");
+                Action<DbContextOptionsBuilder> configureDbContext = o => o.UseSqlite(connectionString);
+
+                services.AddDbContext<YarseyDbContext>(configureDbContext);
+                services.AddSingleton<YarseyDbContextFactory>(new YarseyDbContextFactory(configureDbContext));
+            });
+
+            return host;
+        }
+    }
+}
