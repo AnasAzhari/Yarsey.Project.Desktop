278db2077cb71aeb32ca28e1997f8f1169bca3c4
anasaz85 <4k.anas85@gmail.com>
2021-03-28 13:19:35 +0800

    customer dialog xaml

---
+++ Yarsey.WPF/Views/Modal/CustomerDialogVer2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Yarsey.WPF.Views.Modal
+{
+    /// <summary>
+    /// Interaction logic for CustomerDialogVer2.xaml
+    /// </summary>
+    public partial class CustomerDialogVer2 : Window
+    {
+        public CustomerDialogVer2()
+        {
+            InitializeComponent();
+        }
+    }
+}
