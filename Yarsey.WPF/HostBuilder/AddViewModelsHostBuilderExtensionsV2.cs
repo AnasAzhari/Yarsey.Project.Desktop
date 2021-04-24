595ce2190a0b4d944d0f15ac49424b052bf47659
anasaz85 <4k.anas85@gmail.com>
2021-04-24 22:08:50 +0800

    images and rebuild layout

---
+++ Yarsey.WPF/HostBuilder/AddViewModelsHostBuilderExtensionsV2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Yarsey.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Yarsey.WPF.Stores;
+using Yarsey.WPF.Services;
+using Yarsey.WPF.ViewModels;
+using Yarsey.EntityFramework.Services;
+using Yarsey.WPF.Views;
+using Yarsey.WPF.ViewModels.Modal;
+namespace Yarsey.WPF.HostBuilder
+{
+    public static class AddViewModelsHostBuilderExtensionsV2
+    {
+
+        public static IHostBuilder AddViewModelsV2(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                // services.AddSingleton<INavigationService>(s => CreateHomeNavigationService(s));
+                services.AddSingleton<GeneralModalNavigationService>(s => new GeneralModalNavigationService(s.GetRequiredService<ModalNavigationStore>(), s.GetRequiredService<ErrorMessageViewModel>(), s.GetRequiredService<SuccessMessageViewModel>()));
+                services.AddSingleton<CloseModalNavigationService>();
+             
+
+
+
+                services.AddSingleton<MainWindow>(s => new MainWindow() { DataContext = s.GetRequiredService<MainViewModel>() });
+                services.AddSingleton<CustomerView>();
+
+            });
+
+            return host;
+        }
+
+      
+    }
+}
